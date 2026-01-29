ba20a32761ac5f9e09028e62cc621d103418aada
Alexander_Shapovalov <sasha.shapovalov.06@bk.ru>
2026-01-29 20:37:27 +0300

    .gitignore added

--- CsvMetricsProcessor/obj/Debug/net10.0/CsvMetricsProcessor.AssemblyInfo.cs
+++ CsvMetricsProcessor/obj/Debug/net10.0/CsvMetricsProcessor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CsvMetricsProcessor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c115ffb0cf8bfa5072b4a31f52e84ff75409511")]
 [assembly: System.Reflection.AssemblyProductAttribute("CsvMetricsProcessor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CsvMetricsProcessor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
